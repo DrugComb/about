5a3bc8f2045f04b9eb827c7a4ad1e3a05c1d9e6d
DrugComb <49985456+DrugComb@users.noreply.github.com>
2019-05-11 13:13:09 +0300

    First Commit

---
+++ FA/index.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html>
+<title>DrugComb</title>
+<head>
+      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css?family=News+Cycle:400,700" rel="stylesheet">
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-126984000-1"></script>
+      <script>
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+         
+         gtag('config', 'UA-126984000-1');
+      </script>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+      <link rel="stylesheet" href="../../lastupdate/css/style5.css?version=5">
+      <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+      <script src="../about.js"></script>
+
+<style>
+
+p {
+	color:black;
+font-size: 1.3em;
+   }
+
+
+
+</style>
+<script>
+$(document).ready(function(){
+$('.collapse list-unstyled').click(function(e) {
+    e.stopPropagation();
+});});
+</script>
+        </head>
+
+<body>
+
+
+
+ <nav class="navbar navbar-expand-lg navbar-light bg-light" style="margin-left:0px;">
+         <div class="container-fluid">
+            <div class="row" >
+               <div class="introduction" >
+                  <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+                     -->
+                  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+                  <ul id="header" class="nav navbar-nav ">
+                     <!--title="Hide left Panel" if you want to show twxt on hover of arrow-->
+                     <li id="sidebarCollapse" class="sidearrow" style="font-size:60px;background-color: white;padding-top:11px;">
+                        <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" 
+                           style="pointer-events: none; display: block; width: 100%; height: 100%;">
+                           <g class="style-scope yt-icon">
+                              <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" class="style-scope yt-icon"></path>
+                           </g>
+                        </svg>
+                     </li>
+                     <li class="druglogo"><a style="font-size:20px;background-color: white;padding-top:14px;padding-right:24px;">DrugComb Portal</a></li>
+                     <!--<a id="atag" class="fa fa-angle-double-left"  style="font-size:34px;background-color: white;" ></a>--></li>
+                     <li class=" home" id="home" data-step="1" data-intro="Welcome to DrugComb data portal."><a href="../../" >Home</a></li>
+                     <li id="about" data-step="2" data-intro="Here you can find the contact information for us."
+                        ><a  style="color:blue;">About</a></li>
+                     <li><a href="https://drugcomb.fimm.fi/analysis/"  
+                        data-step="3" data-intro="This is Analysis page to process your data.">Analysis</a></li>
+                     <li><a href="https://drugcomb.fimm.fi/contribute/" >  Contribute  </a></li>
+                     <!-- <li><a href="https://drugcomb.fimm.fi/contact/" data-step="3" data-intro="Here you can find the contact inofrmation for us."
+                        >Contact</a></li>-->
+                     <li><a href="https://drugcomb.fimm.fi/help/" data-step="4" data-intro="This is the documentation page">Help</a></li>
+                  </ul>
+                  <!--
+                     <a class="nav navbar-nav " style="margin-top:0px;margin-right:15px;float:right;" >
+                                   	<img  class="list-group-item" src="eu.jpg" width="60px" height="50px" style="margin-left:70px;float:center;">
+                     
+                     
+                                  </a>
+                     
+                     -->
+					 
+					 
+					  <div style="float: right;margin-top:10px;">
+				 
+				 	<select class="form-control selectpicker populate prolist"  id="lang" name="lang"  onchange="location = this.value;" >
+					 <option class="selected"><a>FA</a></option>
+					    <option value="https://drugcomb.fimm.fi/about/"><a>EN</a></option>
+		 <option value="https://drugcomb.fimm.fi/about/中文/"><a>中文</a></option>
+
+     
+        <option value="https://drugcomb.fimm.fi/about/RU/"><a>RU</a></option>
+        <option value="https://drugcomb.fimm.fi/about/FR/"><a>FR</a></option>
+        <option value="https://drugcomb.fimm.fi/about/IT/"><a>IT</a></option>
+       
+    </select>	
+	 </div>
+               </div>
+            </div>
+         </div>
+      </nav>
+
+
+
+
+
+
+
+
+
+             <div class="wrapper">
+
+        <div id="content">
+
+ <div class="col-xl-6">
+<br>
+
+                                </div>
+
+<div >
+
+		 <div class="col-x1-11">
+
+                                <div class="col-xl-6" >
+
+                <div class="card">
+                                <div class="card-body">
+                    <div class="chart-wrapper mt-4" >
+
+<p dir="rtl" align="justify">	 DrugComb  یک درگاه اینترنتی عمومی و رایگان است که نتایج مطالعات کاوش ترکیب چند دارویی در رده های متنوع سلول های سرطان در آن جمع آوری، استانداردسازی و هماهنگ شده اند (http://drugcomb.fimm.fi). در این درگاه اینترنتی، چندین ابزار محاسباتی به همراه آرایه ای از تصویرسازی داده ها برای تحلیل داده های داروهای ترکیبی فراهم شده است. تمامی داده ها و ابزارهای انفورماتیکی به صورت رایگان در دسترس تمامی محققین سرطان قرار دارد.	</p>
+
+
+                    </div>
+                </div>
+                                </div>
+                                </div>
+        </div>	
+		
+		<div class="col-xl-6">
+<br>
+<br>
+
+                                </div>
+		
+     <div class="col-x1-11">
+
+                                <div class="col-xl-6" >
+
+                <div class="card">
+                                <div class="card-body">
+                    <div class="chart-wrapper mt-4" >
+
+					
+					
+					
+					
+					
+					
+
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+<h3 dir="rtl" align="justify">مقدمه</h3>
+
+        <p dir="rtl" align="justify"> یکی از چالش های اساسی در سامانه بهداشتی-درمانی، درمان کارآمد و اختصاصی مبتلایان به سرطان است. اگرچه داروهای زیادی برای این منظور در درمان استفاده می شوند، اما این داروها کارآمدی پایینی داشته و هنوز نمی دانیم که چرا بسیاری از بیماران با استفاده از این داروها درمان نمی شوند. حتی در مواقعی که یک پاسخ درمانی اولیه از یک دارو مشاهده می کنیم، به علت توانایی بالای جهش¬زایی و فعالیت بالای سلول های سرطانی، این سلول ها با فعال کردن مسیرهای جبرانی نسبت به داور مقاومت نشان می دهند. برای دستیابی به پاسخ های درمانی پایدار و کارآمد، ما به استفاده از رویکرد ترکیب داروها با اهداف چندگانه نیازمندیم تا سلول های سرطانی را به صورت انتخابی مهار کرده و از بروز مقاومت دارویی جلوگیری نماییم. </p>
+
+
+
+                    </div>
+                </div>
+                                </div>
+                                </div>
+        </div>
+
+
+ <div class="col-xl-6">
+<br>
+
+                                </div>
+
+								 <div class="col-x1-11">
+
+                                <div class="col-xl-6" >
+
+                <div class="card">
+                                <div class="card-body">
+                    <div class="chart-wrapper mt-4" >
+
+<h3 dir="rtl" align="justify">راهبرد پژوهشی</h3>
+
+                                                <p dir="rtl" align="justify">
+												
+												به منظور تسهیل یک طراحی منطقی از ترکیب چند دارو در راستای پزشکی فردی شده سرطان، ما یک روش خوشه بندی برای تعیین زیرگروه های بیماران بر مبنای درمان اختصاصی (داروی درست برای بیمار مشخص) توسعه دادیم. در این پژوهش، یک رویکرد مدلسازی شبکه ای برای مقاومت بیماران به شیمی درمانی توسعه داده شده است که بهترین ترکیب چند دارویی را پیش بینی می کند. پیش بینی ترکیب چند دارویی برای هر بیمار به صورت جداگانه انجام گرفته و به کمک پلتفرم آزمون دارو روی نمونه های بیماران پیش از درمانگاه، ارزیابی می گردد. 
+سپس داده های به دست آمده به منظور تعیین هم¬افزایی قابل توجه در دوزهای درمانی مربوطه مورد بررسی قرار گرفته و به منظور تفسیر مکانیسم عمل، ترکیب چند دارویی موردنظر در شبکه پیام رسانی سرطان مدلسازی می شود. ترکیب های با بالاترین کارآمدی در هر نمونه بیمار یا زیرگروهی از بیماران برای استفاده در گزینه های درمانی اضافه می گردند. این فرایند پیش بینی، مدلسازی و آزمون ترکیب چند دارویی با توجه به مشکلات روش های درمانی رایج تک دارویی و دارای سمیت سلولی، می تواند منجر به روش درمانی جدید، ایمن تر و با کارایی بیشتر بگردد. 
+
+												
+												</p>
+
+
+                    </div>
+                </div>
+                                </div>
+                                </div>
+        </div>
+
+
+
+		
+		
+		
+		
+		
+		 <div class="col-xl-6">
+<br>
+
+                                </div>
+		
+		
+		
+		
+		 <div class="col-x1-11">
+
+                                <div class="col-xl-6" >
+
+                <div class="card">
+                                <div class="card-body">
+                    <div class="chart-wrapper mt-4" >
+
+<h3 dir="rtl" >منابع</h3>
+
+                                                <p>		
+[1] DrugComb - an integrative cancer drug combination data portal.
+Zagidullin, B., Aldahdooh, J. Malyutina, A., Wang, W., Wang, Y., Saad, J., Pessia, A. and Tang. J. Manuscript.
+ <p>	[2] Malyutina, A., Majumder, M.M., Wang, W., Pessia, A., Heckman, C. and Tang, J. Drug combination sensitivity scoring facilitates the discovery of synergistic and efficacious drug combinations in cancer. Under Review.</p>
+ <p>	[3] Wang, W., Malyutina, A., Pessia, A., Heckman, C. and Tang, J. Improving the consistency of functional genomics screens using molecular features - a multi-omics, pan-cancer study. Under Review.</p>
+ <p>	[4] Tang, J. (2017) Informatics approaches for predicting, understanding and testing cancer drug combinations. Methods Mol. Biol. 1636:485-506. Book chapter in Kinase Signaling Networks, Springer</p>
+ <p>	[5] Ianevski, A., He, L., Aittokallio, T. and Tang, J. (2017) SynergyFinder: a web application for analyzing drug combination dose-response matrix data. Bioinformatics. 33, 2413-2415 </p>
+ <p>	[6] He, L., Kulesskiy, E., Saarela, J., Turunen, L., Wennerberg, K., Aittokallio, T. and Tang, J. (2017) Methods for high-throughput drug combination screening and synergy scoring. Methods Mol. Biol. 1711:351-398. Book chapter in Cancer Systems Biology, Springer.</p> 
+ <p>	[7] Yadav, B., Wenerberg, K., Aittokallio, T. and Tang, J. (2015) Searching for drug synergy in complex dose-response landscapes using an interaction potency model. Comput. Struct. Biotechnol. J. 13, 504-513 </p>
+ <p>	[8] Tang, J., Wennerberg, K. and Aittokallio, T. (2015) What is synergy? The Saariselkä agreement revisited.  Front. Pharmacol. 6, 181 
+		
+</p>
+
+
+                    </div>
+                </div>
+                                </div>
+                                </div>
+        </div>
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+
+
+
+
+            </div> <!--/.col-sm-4 -->
+
+ </div>
+
+
+
+
+                                </div>
+        </body>
+</html>
+
