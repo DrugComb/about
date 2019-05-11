d4d47cd806c47ba2daac0d9f3a9470ae5a0405b7
DrugComb <49985456+DrugComb@users.noreply.github.com>
2019-05-11 13:13:48 +0300

    First Commit

---
+++ FR/index.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html>
+<html>
+<title>DrugComb</title>
+<head>
+          <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css?family=News+Cycle:400,700" rel="stylesheet">
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-126984000-1"></script>
+      <script>
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+         
+         gtag('config', 'UA-126984000-1');
+      </script>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+      <link rel="stylesheet" href="../../lastupdate/css/style5.css?version=5">
+      <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+      <script src="../about.js"></script>
+
+<style>
+
+p {
+	color:black;
+font-size: 1.3em;
+   }
+
+
+</style>
+
+        </head>
+
+<body>
+
+ <nav class="navbar navbar-expand-lg navbar-light bg-light" style="margin-left:0px;">
+         <div class="container-fluid">
+            <div class="row" >
+               <div class="introduction" >
+                  <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+                     -->
+                  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+                  <ul id="header" class="nav navbar-nav ">
+                     <!--title="Hide left Panel" if you want to show twxt on hover of arrow-->
+                     <li id="sidebarCollapse" class="sidearrow" style="font-size:60px;background-color: white;padding-top:11px;">
+                        <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" 
+                           style="pointer-events: none; display: block; width: 100%; height: 100%;">
+                           <g class="style-scope yt-icon">
+                              <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" class="style-scope yt-icon"></path>
+                           </g>
+                        </svg>
+                     </li>
+                     <li class="druglogo"><a style="font-size:20px;background-color: white;padding-top:14px;padding-right:24px;">DrugComb Portal</a></li>
+                     <!--<a id="atag" class="fa fa-angle-double-left"  style="font-size:34px;background-color: white;" ></a>--></li>
+                     <li class=" home" id="home" data-step="1" data-intro="Welcome to DrugComb data portal."><a href="../../" >Home</a></li>
+                     <li id="about" data-step="2" data-intro="Here you can find the contact information for us."
+                        ><a  style="color:blue;">About</a></li>
+                     <li><a href="https://drugcomb.fimm.fi/analysis/"  
+                        data-step="3" data-intro="This is Analysis page to process your data.">Analysis</a></li>
+                     <li><a href="https://drugcomb.fimm.fi/contribute/" >  Contribute  </a></li>
+                     <!-- <li><a href="https://drugcomb.fimm.fi/contact/" data-step="3" data-intro="Here you can find the contact inofrmation for us."
+                        >Contact</a></li>-->
+                     <li><a href="https://drugcomb.fimm.fi/help/" data-step="4" data-intro="This is the documentation page">Help</a></li>
+                  </ul>
+                  <!--
+                     <a class="nav navbar-nav " style="margin-top:0px;margin-right:15px;float:right;" >
+                                   	<img  class="list-group-item" src="eu.jpg" width="60px" height="50px" style="margin-left:70px;float:center;">
+                     
+                     
+                                  </a>
+                     
+                     -->
+					 
+					 
+					 <div style="float: right;margin-top:10px;">
+				 
+				 	<select class="form-control selectpicker populate prolist"  id="lang" name="lang"  onchange="location = this.value;" >
+					 <option class="selected"><a>FR</a></option>
+					    <option value="https://drugcomb.fimm.fi/about/"><a>EN</a></option>
+		 <option value="https://drugcomb.fimm.fi/about/中文/"><a>中文</a></option>
+
+     
+        <option value="https://drugcomb.fimm.fi/about/RU/"><a>RU</a></option>
+        <option  value="https://drugcomb.fimm.fi/about/FA/"><a>FA</a></option>
+        <option value="https://drugcomb.fimm.fi/about/IT/"><a>IT</a></option>
+       
+    </select>	
+	 </div>
+               </div>
+            </div>
+         </div>
+      </nav>
+
+	  
+	  
+	  
+	  
+        <div class="wrapper">
+
+        <div id="content">
+
+			
+			 <div class="col-xl-6">
+<br>
+
+                                </div>
+			
+<div >
+
+		 <div class="col-x1-11">
+
+                                <div class="col-xl-6" >
+
+                <div class="card">
+                                <div class="card-body">
+                    <div class="chart-wrapper mt-4" >
+
+<p align="justify">	DrugComb http://drugcomb.fimm.fi est un portail de données en libre accès, où les résultats des études de dépistage de combinaisons de médicaments pour une grande variété de lignées de cellules cancéreuses sont accumulés, normalisés et harmonisés. Une large sélection d'outils de visualisation et de calcul en expansion continue est fournie aux utilisateurs pour l'analyse des données de combinaison de médicaments. Toutes les données et tous les outils informatiques sont rendus gratuitement accessible à une vaste communauté de chercheurs sur le cancer.		</p>
+
+
+                    </div>
+                </div>
+                                </div>
+                                </div>
+        </div>	
+		
+		<div class="col-xl-6">
+<br>
+
+                                </div>
+		
+     <div class="col-x1-11">
+
+                                <div class="col-xl-6" >
+
+                <div class="card">
+                                <div class="card-body">
+                    <div class="chart-wrapper mt-4" >
+
+					
+					
+					
+					
+					
+					
+
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+<h3>Contexte </h3>
+                                                <p align="justify">Rendre le traitement du cancer plus personnalisé et plus efficace constitue l’un des plus grands défis de notre système de santé. Actuellement, de nombreux médicaments en usage clinique font preuve d’une efficacité très limitée auprès de certains groupes de patients, et cela pour des raisons toujours inconnues. Même en cas de réponse initiale au traitement, les cellules cancéreuses à potentiel mutationnel élevé et à redondance fonctionnelle peuvent facilement développer une pharmacorésistance par le biais d'une activation de multiples mécanismes de compensation. Par conséquent, la réalisation de réponses cliniques durables et efficaces nécessite des combinaisons de médicaments à cibles multiples, capables d’inhiber sélectivement les cellules cancéreuses et de bloquer ainsi l’émergence d’une résistance aux médicaments. La plateforme ‘Individualized Systems Medicine’ (ISM) [en français : la médecine des systèmes individualisés] à FIMM songe à identifier de nouvelles possibilités thérapeutiques susceptibles d'être traduits en usage clinique. Pour ce faire, des spécimens de patients atteints de cancer sont d’abord prélevés dans des cliniques et puis cultivés au laboratoire pour des tests de sensibilité aux médicaments et pour l'établissement de leurs profils moléculaires. Puisqu’il est impossible de tester de manière exhaustive toutes les combinaisons de médicaments pour chaque type de cancer ou pour chaque patient, la mise en œuvre de méthodes informatiques offre une efficacité accrue pour prévoir les combinaisons de médicaments les plus performantes.</p>
+
+
+
+                    </div>
+                </div>
+                                </div>
+                                </div>
+        </div>
+
+
+ <div class="col-xl-6">
+<br>
+
+                                </div>
+
+								 <div class="col-x1-11">
+
+                                <div class="col-xl-6" >
+
+                <div class="card">
+                                <div class="card-body">
+                    <div class="chart-wrapper mt-4" >
+
+<h3>Stratégie de Recherche</h3>
+
+                                                <p align="justify">Afin de faciliter la conception rationnelle de combinaisons de médicaments permettant d’atteindre une médecine anticancéreuse réellement personnalisée, nous développerons des méthodes de classification pour l'identification de sous-groupes de patients nécessitant des traitement particuliers (« le bon médicament pour le bon patient »). Pour les patients résistant à la chimiothérapie, nous mettrons en place des approches de modélisation de réseaux d’interactions biologiques dans le but de prédire les combinaisons de médicaments qui seront les plus performantes. La prédiction des combinaisons de médicaments se fera pour chacun des patients, et sera validée à l'aide d'une plateforme qui évaluera ex vivo l’activité de ces combinaisons sur des échantillons prélevés des patients. Ensuite, nous explorerons les résultats de l’activité de ces combinaisons pour identifier d’éventuelles synergies significatives pour des doses thérapeutiques. Les meilleures combinaisons de médicaments testées seront aussi modélisées dans des réseaux de signalisation du cancer pour en déduire leurs mécanismes d'action. Ainsi, les combinaisons de médicaments présentant une efficacité sélective dans des échantillons de patients individuels ou dans des sous-groupes d'échantillons seront traduites en de potentielles options de traitement. Le pipeline complet proposé pour la prédiction, la modélisation, et l’effectuation de tests de combinaisons de médicaments pourrait alors déboucher sur de nouveaux traitements plus efficaces et plus sûrs par rapport aux monothérapies cytotoxiques actuelles.</p>
+
+
+                    </div>
+                </div>
+                                </div>
+                                </div>
+        </div>
+
+
+
+		
+		
+		
+		
+		
+		
+		
+		 <div class="col-xl-6">
+<br>
+
+                                </div>
+		
+		
+		 <div class="col-x1-11">
+
+                                <div class="col-xl-6" >
+
+                <div class="card">
+                                <div class="card-body">
+                    <div class="chart-wrapper mt-4" >
+
+<h3>Références</h3>
+
+                                                <p align="justify">		
+[1] DrugComb - an integrative cancer drug combination data portal.
+Zagidullin, B., Aldahdooh, J. Malyutina, A., Wang, W., Wang, Y., Saad, J., Pessia, A. and Tang. J. Manuscript.
+ <p align="justify">	[2] Malyutina, A., Majumder, M.M., Wang, W., Pessia, A., Heckman, C. and Tang, J. Drug combination sensitivity scoring facilitates the discovery of synergistic and efficacious drug combinations in cancer. Under Review.</p>
+ <p align="justify">	[3] Wang, W., Malyutina, A., Pessia, A., Heckman, C. and Tang, J. Improving the consistency of functional genomics screens using molecular features - a multi-omics, pan-cancer study. Under Review.</p>
+ <p align="justify">	[4] Tang, J. (2017) Informatics approaches for predicting, understanding and testing cancer drug combinations. Methods Mol. Biol. 1636:485-506. Book chapter in Kinase Signaling Networks, Springer</p>
+ <p align="justify">	[5] Ianevski, A., He, L., Aittokallio, T. and Tang, J. (2017) SynergyFinder: a web application for analyzing drug combination dose-response matrix data. Bioinformatics. 33, 2413-2415 </p>
+ <p align="justify">	[6] He, L., Kulesskiy, E., Saarela, J., Turunen, L., Wennerberg, K., Aittokallio, T. and Tang, J. (2017) Methods for high-throughput drug combination screening and synergy scoring. Methods Mol. Biol. 1711:351-398. Book chapter in Cancer Systems Biology, Springer.</p> 
+ <p align="justify">	[7] Yadav, B., Wenerberg, K., Aittokallio, T. and Tang, J. (2015) Searching for drug synergy in complex dose-response landscapes using an interaction potency model. Comput. Struct. Biotechnol. J. 13, 504-513 </p>
+ <p align="justify">	[8] Tang, J., Wennerberg, K. and Aittokallio, T. (2015) What is synergy? The Saariselkä agreement revisited.  Front. Pharmacol. 6, 181 
+		
+</p>
+
+
+                    </div>
+                </div>
+                                </div>
+                                </div>
+        </div>
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+
+
+
+
+            </div> <!--/.col-sm-4 -->
+
+ </div>
+
+
+
+
+                                </div>
+        </body>
+</html>
+
