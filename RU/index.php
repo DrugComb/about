aadf76a4fb9d798f8cd95ef7b50afa62dd6ea1b0
DrugComb <49985456+DrugComb@users.noreply.github.com>
2019-05-11 13:14:11 +0300

    First Commit

---
+++ RU/index.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html>
+<title>DrugComb</title>
+<head>
+      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css?family=News+Cycle:400,700" rel="stylesheet">
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-126984000-1"></script>
+      <script>
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+         
+         gtag('config', 'UA-126984000-1');
+      </script>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+      <link rel="stylesheet" href="../../lastupdate/css/style5.css?version=5">
+      <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+      <script src="../about.js"></script>
+
+<style>
+
+p {
+		color:black;
+
+font-size: 1.3em;
+   }
+
+
+</style>
+
+        </head>
+
+<body>
+
+
+ <nav class="navbar navbar-expand-lg navbar-light bg-light" style="margin-left:0px;">
+         <div class="container-fluid">
+            <div class="row" >
+               <div class="introduction" >
+                  <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+                     -->
+                  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+                  <ul id="header" class="nav navbar-nav ">
+                     <!--title="Hide left Panel" if you want to show twxt on hover of arrow-->
+                     <li id="sidebarCollapse" class="sidearrow" style="font-size:60px;background-color: white;padding-top:11px;">
+                        <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" 
+                           style="pointer-events: none; display: block; width: 100%; height: 100%;">
+                           <g class="style-scope yt-icon">
+                              <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" class="style-scope yt-icon"></path>
+                           </g>
+                        </svg>
+                     </li>
+                     <li class="druglogo"><a style="font-size:20px;background-color: white;padding-top:14px;padding-right:24px;">DrugComb Portal</a></li>
+                     <!--<a id="atag" class="fa fa-angle-double-left"  style="font-size:34px;background-color: white;" ></a>--></li>
+                     <li class=" home" id="home" data-step="1" data-intro="Welcome to DrugComb data portal."><a href="../../" >Home</a></li>
+                     <li id="about" data-step="2" data-intro="Here you can find the contact information for us."
+                        ><a  style="color:blue;">About</a></li>
+                     <li><a href="https://drugcomb.fimm.fi/analysis/"  
+                        data-step="3" data-intro="This is Analysis page to process your data.">Analysis</a></li>
+                     <li><a href="https://drugcomb.fimm.fi/contribute/" >  Contribute  </a></li>
+                     <!-- <li><a href="https://drugcomb.fimm.fi/contact/" data-step="3" data-intro="Here you can find the contact inofrmation for us."
+                        >Contact</a></li>-->
+                     <li><a href="https://drugcomb.fimm.fi/help/" data-step="4" data-intro="This is the documentation page">Help</a></li>
+                  </ul>
+                  <!--
+                     <a class="nav navbar-nav " style="margin-top:0px;margin-right:15px;float:right;" >
+                                   	<img  class="list-group-item" src="eu.jpg" width="60px" height="50px" style="margin-left:70px;float:center;">
+                     
+                     
+                                  </a>
+                     
+                     -->
+					 
+					 
+					  <div style="float: right;margin-top:10px;">
+				 
+				 	<select class="form-control selectpicker populate prolist"  id="lang" name="lang"  onchange="location = this.value;" >
+					 <option class="selected"><a>RU</a></option>
+					    <option value="https://drugcomb.fimm.fi/about/"><a>EN</a></option>
+		 <option value="https://drugcomb.fimm.fi/about/中文/"><a>中文</a></option>
+
+     
+        <option  value="https://drugcomb.fimm.fi/about/FA/"><a>FA</a></option>
+        <option value="https://drugcomb.fimm.fi/about/FR/"><a>FR</a></option>
+        <option value="https://drugcomb.fimm.fi/about/IT/"><a>IT</a></option>
+       
+    </select>	
+	 </div>
+               </div>
+            </div>
+         </div>
+      </nav>
+	  
+
+	  
+	  
+	  
+        <div class="wrapper">
+
+        <div id="content">
+
+
+		<div class="col-xl-6">
+<br>
+</div>	
+			
+			
+			
+<div >
+
+		 <div class="col-x1-11">
+
+                                <div class="col-xl-6" >
+
+                <div class="card">
+                                <div class="card-body">
+                    <div class="chart-wrapper mt-4" >
+
+<p align="justify">	DrugComb — это открытый портал, где собираются, стандартизируются и унифицируются результаты исследований по скринингу комбинаций лекарственных препаратов на широком спектре раковых клеточных линий. В рамках данного проекта мы также предоставляем в открытом доступе все инструменты для анализа и визуализации данных. 		</p>
+
+
+                    </div>
+                </div>
+                                </div>
+                                </div>
+        </div>	
+		
+<div class="col-xl-6">
+<br>
+</div>
+		
+     <div class="col-x1-11">
+
+                                <div class="col-xl-6" >
+
+                <div class="card">
+                                <div class="card-body">
+                    <div class="chart-wrapper mt-4" >
+
+					
+					
+					
+					
+					
+					
+
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+<h3>Теоретические основы</h3>
+                                                <p align="justify">На данный момент развитие более эффективных и, в будущем, персонализированных противораковых терапевтических средств является одной из главных задач в лечение раковых заболеваний. Большой процент лекарственных препаратов, поступающих в клиническую практику, имеет ограниченную эффективность, а научное общество не обладает достаточно глубоким пониманием того, почему определенные пациенты являются нон-респондентами. Даже при наличии первоначальной позитивной реакции на терапевтическое вмешательство, раковые клетки с высоким мутагенным потенциалом и функциональной избыточностью легко развивают иммунологическую толерантность путем активации компенсирующих метаболических путей. Для достижения эффективного и устойчивого клинического отклика необходимы комбинации лекарств способные селективно ингибировать раковые клетки, одновременно блокируя возникновение лекарственной резистентности. Платформа Индивидуализированной Системной Медицины (ISM) в Институте Молекулярной Медицины Финляндии (FIMM) ставит своей целью выявление новых комбинаций лекарств с максимально высоким потенциалом использования в клинических реалиях. Образцы раковых клеток пациентов собираются и культивируются in-vitro для тестирования восприимчивости к лекарственным препаратам и последующего молекулярного профилирования. Поскольку исчерпывающий экспериментальный анализ всех возможных комбинаций лекарств для каждого конкретного типа рака и/или пациента невозможен, необходимо использовать методы математического моделирования для повышения результативности прогнозирования эффективных комбинаций лекарств.</p>
+
+
+
+                    </div>
+                </div>
+                                </div>
+                                </div>
+        </div>
+
+
+ <div class="col-xl-6">
+<br>
+
+                                </div>
+
+								 <div class="col-x1-11">
+
+                                <div class="col-xl-6" >
+
+                <div class="card">
+                                <div class="card-body">
+                    <div class="chart-wrapper mt-4" >
+
+<h3>Стратегия исследования</h3>
+
+                                                <p align="justify">Для упрощения рациональной разработки комбинаций лекарственных препаратов и де факто персонализированной противораковой терапии, мы работаем над идентификацией подгрупп пациентов, которым требуется индивидуальное лечение («правильное лекарство для правильного пациента»). Для пациентов устойчивых к химиотерапии мы разрабатываем подходы системного моделирования для предсказания потенциально наиболее эффективных комбинаций лекарственных препаратов. Предсказание подобных комбинаций будет сделано для каждого пациента с последующей проверкой на преклинической платформе тестирования лекарств. Мы изучим данные скрининга комбинаций лекарств для определения эффекта взаимного усиления терапевтического эффекта в терапевтически значимых диапазонах доз. Механизмы действия наиболее перспективных комбинаций лекарств будут смоделированы в сетях межклеточной коммуникации раковых опухолей. Комбинации лекарств с выборочно высокой эффективностью, проверенной на отдельных образцах пациентов или на их подгруппах, будут использованы для разработки клинических методов лечения. Мы надеемся, что данный метод прогнозирования комбинаций лекарственных средств приведет к открытию новых, более эффективных и безопасных методов лечения по сравнению с ныне существующими цитотоксическими монотерапиями.</p>
+
+
+                    </div>
+                </div>
+                                </div>
+                                </div>
+        </div>
+
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		<div class="col-xl-6">
+<br>
+</div>
+		
+		 <div class="col-x1-11">
+
+                                <div class="col-xl-6" >
+
+                <div class="card">
+                                <div class="card-body">
+                    <div class="chart-wrapper mt-4" >
+
+<h3>Использованная литература</h3>
+
+                                                <p align="justify">		
+[1] DrugComb - an integrative cancer drug combination data portal.
+Zagidullin, B., Aldahdooh, J. Malyutina, A., Wang, W., Wang, Y., Saad, J., Pessia, A. and Tang. J. Manuscript.
+ <p align="justify">	[2] Malyutina, A., Majumder, M.M., Wang, W., Pessia, A., Heckman, C. and Tang, J. Drug combination sensitivity scoring facilitates the discovery of synergistic and efficacious drug combinations in cancer. Under Review.</p>
+ <p align="justify">	[3] Wang, W., Malyutina, A., Pessia, A., Heckman, C. and Tang, J. Improving the consistency of functional genomics screens using molecular features - a multi-omics, pan-cancer study. Under Review.</p>
+ <p align="justify">	[4] Tang, J. (2017) Informatics approaches for predicting, understanding and testing cancer drug combinations. Methods Mol. Biol. 1636:485-506. Book chapter in Kinase Signaling Networks, Springer</p>
+ <p align="justify">	[5] Ianevski, A., He, L., Aittokallio, T. and Tang, J. (2017) SynergyFinder: a web application for analyzing drug combination dose-response matrix data. Bioinformatics. 33, 2413-2415 </p>
+ <p align="justify">	[6] He, L., Kulesskiy, E., Saarela, J., Turunen, L., Wennerberg, K., Aittokallio, T. and Tang, J. (2017) Methods for high-throughput drug combination screening and synergy scoring. Methods Mol. Biol. 1711:351-398. Book chapter in Cancer Systems Biology, Springer.</p> 
+ <p align="justify">	[7] Yadav, B., Wenerberg, K., Aittokallio, T. and Tang, J. (2015) Searching for drug synergy in complex dose-response landscapes using an interaction potency model. Comput. Struct. Biotechnol. J. 13, 504-513 </p>
+ <p align="justify">	[8] Tang, J., Wennerberg, K. and Aittokallio, T. (2015) What is synergy? The Saariselkä agreement revisited.  Front. Pharmacol. 6, 181 
+		
+</p>
+
+
+                    </div>
+                </div>
+                                </div>
+                                </div>
+        </div>
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+
+
+
+
+            </div> <!--/.col-sm-4 -->
+
+ </div>
+
+
+
+
+                                </div>
+        </body>
+</html>
+
