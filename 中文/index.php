fcac0175610e409d53f22c364a23c9bb0b588952
DrugComb <49985456+DrugComb@users.noreply.github.com>
2019-05-11 13:14:21 +0300

    First Commit

---
+++ 中文/index.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html>
+<title>DrugComb</title>
+<head>
+      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css?family=News+Cycle:400,700" rel="stylesheet">
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-126984000-1"></script>
+      <script>
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+         
+         gtag('config', 'UA-126984000-1');
+      </script>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+      <link rel="stylesheet" href="../../lastupdate/css/style5.css?version=5">
+      <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+      <script src="../about.js"></script>
+
+<style>
+
+p {
+		color:black;
+
+font-size: 1.3em;
+   }
+
+
+</style>
+
+        </head>
+
+<body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="margin-left:0px;">
+         <div class="container-fluid">
+            <div class="row" >
+               <div class="introduction" >
+                  <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+                     -->
+                  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+                  <ul id="header" class="nav navbar-nav ">
+                     <!--title="Hide left Panel" if you want to show twxt on hover of arrow-->
+                     <li id="sidebarCollapse" class="sidearrow" style="font-size:60px;background-color: white;padding-top:11px;">
+                        <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" 
+                           style="pointer-events: none; display: block; width: 100%; height: 100%;">
+                           <g class="style-scope yt-icon">
+                              <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" class="style-scope yt-icon"></path>
+                           </g>
+                        </svg>
+                     </li>
+                     <li class="druglogo"><a style="font-size:20px;background-color: white;padding-top:14px;padding-right:24px;">DrugComb Portal</a></li>
+                     <!--<a id="atag" class="fa fa-angle-double-left"  style="font-size:34px;background-color: white;" ></a>--></li>
+                     <li class=" home" id="home" data-step="1" data-intro="Welcome to DrugComb data portal."><a href="../../" >Home</a></li>
+                     <li id="about" data-step="2" data-intro="Here you can find the contact information for us."
+                        ><a  style="color:blue;">About</a></li>
+                     <li><a href="https://drugcomb.fimm.fi/analysis/"  
+                        data-step="3" data-intro="This is Analysis page to process your data.">Analysis</a></li>
+                     <li><a href="https://drugcomb.fimm.fi/contribute/" >  Contribute  </a></li>
+                     <!-- <li><a href="https://drugcomb.fimm.fi/contact/" data-step="3" data-intro="Here you can find the contact inofrmation for us."
+                        >Contact</a></li>-->
+                     <li><a href="https://drugcomb.fimm.fi/help/" data-step="4" data-intro="This is the documentation page">Help</a></li>
+                  </ul>
+                  <!--
+                     <a class="nav navbar-nav " style="margin-top:0px;margin-right:15px;float:right;" >
+                                   	<img  class="list-group-item" src="eu.jpg" width="60px" height="50px" style="margin-left:70px;float:center;">
+                     
+                     
+                                  </a>
+                     
+                     -->
+					 
+					 
+					  <div style="float: right;margin-top:10px;">
+				 
+				 	<select class="form-control selectpicker populate prolist"  id="lang" name="lang"  onchange="location = this.value;" >
+							 <option class="selected" ><a>中文</a></option>
+
+        <option value="https://drugcomb.fimm.fi/about/"><a>EN</a></option>
+        <option  value="https://drugcomb.fimm.fi/about/FA/"><a>FA</a></option>
+        <option value="https://drugcomb.fimm.fi/about/FR/"><a>FR</a></option>
+        <option value="https://drugcomb.fimm.fi/about/IT/"><a>IT</a></option>
+        <option value="https://drugcomb.fimm.fi/about/RU/"><a>RU</a></option>
+    </select>	
+	 </div>
+	 
+               </div>
+            </div>
+         </div>
+      </nav>
+	  
+	  
+	  
+	  
+        <div class="wrapper">
+
+        <div id="content">
+
+
+			   
+			   
+
+			
+			
+	<div class="col-xl-6">
+<br>
+
+                                </div>		
+			
+<div >
+
+		 <div class="col-x1-11">
+
+                                <div class="col-xl-6" >
+
+                <div class="card">
+                                <div class="card-body">
+                    <div class="chart-wrapper mt-4" >
+
+<p align="justify">	DrugComb (http://drugcomb.fimm.fi) 是一个开放的面向广大用户的数据门户。该数据库把一系列细胞系的组合用药筛选结果汇总并标准化。同时，该数据门户提供了一系列数据可视化和计算分析工具来分析药物组合结果。所有的数据和信息工具均可免费用于癌症研究。 		</p>
+
+
+                    </div>
+                </div>
+                                </div>
+                                </div>
+        </div>	
+		
+		<div class="col-xl-6">
+<br>
+
+                                </div>
+		
+     <div class="col-x1-11">
+
+                                <div class="col-xl-6" >
+
+                <div class="card">
+                                <div class="card-body">
+                    <div class="chart-wrapper mt-4" >
+
+					
+					
+					
+					
+					
+					
+
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+<h3>背景</h3>
+                                                <p align="justify">更精准、更有效的癌症治疗是当今医疗系统的重大挑战之一。但是，目前许多临床药物的效果有限。而且，人类至今也无法解释个别病人对药物无效的现象。另一方面，即使在最开始有效，易突变的癌症细胞很容易通过激活补偿通路而产生耐药性，使得许多最开始有效的药物无效化。</p>
+
+
+
+                    </div>
+                </div>
+                                </div>
+                                </div>
+        </div>
+
+
+ <div class="col-xl-6">
+<br>
+
+                                </div>
+
+								 <div class="col-x1-11">
+
+                                <div class="col-xl-6" >
+
+                <div class="card">
+                                <div class="card-body">
+                    <div class="chart-wrapper mt-4" >
+
+<h3>研究策略</h3>
+
+                                                <p align="justify">为了确保药物有持续的临床效果，我们需要针对多靶标进行组合用药，从而选择性抑制癌症细胞同时避免耐药性的发生。 FIMM的The Individualized Systems Medicine (ISM，个性化系统医疗平台) 旨在发掘可用于临床的全新的治疗方案。我们首先从临床采集癌症病人的样本，培养后测定样本的药物的敏感性和组学特征。然而，受条件限制，在每个病人样本上都进行所有药物组合的生物实验是不现实的。因此，我们开发了计算方法来预测最有潜力的药物组合，从而提高效率。
+为了设计合理的个性化给药的药物组合，我们将开发基于模型的聚类方法将病人分类，不同类型病人采用不同治疗方法从而实现因人给药。 对于化学疗法耐药的病人，我们将建立网络模型来预测潜在的药物组合。
+我们将为每个病人提供药物组合的个性化预测，然后把预测的潜在药物组合在临床前测试平台的病人样本上进行验证。测试后，我们基于药物组合筛选数据来进一步分析，以探索出治疗浓度下的最佳药物组合。预测的药物组合还将进一步与癌症信号网络整合，以探索其作用机制。最终那些对特定病人有选择性效果并经过临床前验证药物组合将被转化成治疗手段。相比于目前的细胞毒性药物和单靶标治疗药物，我们所提出来的药物组合预测、建模和测试流程将提供全新的、更有效的、更安全的治疗手段。
+</p>
+
+
+                    </div>
+                </div>
+                                </div>
+                                </div>
+        </div>
+
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		 <div class="col-x1-11">
+
+                                <div class="col-xl-6" >
+
+                <div class="card">
+                                <div class="card-body">
+                    <div class="chart-wrapper mt-4" >
+
+<h3>References</h3>
+
+                                                <p align="justify">		
+[1] DrugComb - an integrative cancer drug combination data portal.
+Zagidullin, B., Aldahdooh, J. Malyutina, A., Wang, W., Wang, Y., Saad, J., Pessia, A. and Tang. J. Manuscript.
+ <p align="justify">	[2] Malyutina, A., Majumder, M.M., Wang, W., Pessia, A., Heckman, C. and Tang, J. Drug combination sensitivity scoring facilitates the discovery of synergistic and efficacious drug combinations in cancer. Under Review.</p>
+ <p align="justify">	[3] Wang, W., Malyutina, A., Pessia, A., Heckman, C. and Tang, J. Improving the consistency of functional genomics screens using molecular features - a multi-omics, pan-cancer study. Under Review.</p>
+ <p align="justify">	[4] Tang, J. (2017) Informatics approaches for predicting, understanding and testing cancer drug combinations. Methods Mol. Biol. 1636:485-506. Book chapter in Kinase Signaling Networks, Springer</p>
+ <p align="justify">	[5] Ianevski, A., He, L., Aittokallio, T. and Tang, J. (2017) SynergyFinder: a web application for analyzing drug combination dose-response matrix data. Bioinformatics. 33, 2413-2415 </p>
+ <p align="justify">	[6] He, L., Kulesskiy, E., Saarela, J., Turunen, L., Wennerberg, K., Aittokallio, T. and Tang, J. (2017) Methods for high-throughput drug combination screening and synergy scoring. Methods Mol. Biol. 1711:351-398. Book chapter in Cancer Systems Biology, Springer.</p> 
+ <p align="justify">	[7] Yadav, B., Wenerberg, K., Aittokallio, T. and Tang, J. (2015) Searching for drug synergy in complex dose-response landscapes using an interaction potency model. Comput. Struct. Biotechnol. J. 13, 504-513 </p>
+ <p align="justify">	[8] Tang, J., Wennerberg, K. and Aittokallio, T. (2015) What is synergy? The Saariselkä agreement revisited.  Front. Pharmacol. 6, 181 
+		
+</p>
+
+
+                    </div>
+                </div>
+                                </div>
+                                </div>
+        </div>
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+
+
+
+
+            </div> <!--/.col-sm-4 -->
+
+ </div>
+
+
+
+
+                                </div>
+        </body>
+</html>
+
